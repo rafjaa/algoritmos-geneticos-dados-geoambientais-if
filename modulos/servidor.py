161c386a38869e7cdf9422fa2c83a622deda58cc
Flavio Lucio De P.G Junior <flpgjr@gmail.com>
2014-06-07 11:20:45 -0300

    Update servidor.py
    Chamada dos gráficos implementada

--- modulos/servidor.py
+++ modulos/servidor.py
@@ -1,11 +1,38 @@
 #coding: utf-8
 
 from flask import Flask
+from flask import jsonify, request
+import geneticista
+import graficos
+import json
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/genesis', methods=['POST'])
+def genesis():
+    if request.method == 'POST':
+        tamanho_populacao = request.args.post('tamanho_populacao')
+        numero_geracoes = request.args.post('numero_geracoes')
+        percentual_mutacao = request.args.post('percentual_populacao')
+        ponto_inicial = request.args.post('ponto_inicial')
 
-if __name__ == "__main__":
+        populacao = geneticista.Populacao(tamanho_populacao, numero_geracoes, percentual_mutacao, ponto_inicial)
+        return ''
+
+@app.route('/grafico/geracoes')
+def grafico_geracoes():
+    geracoes = json.load(open('temp/geracoes.json'))
+    graficos.grafico_geracoes(geracoes)
+    return ''
+
+@app.route('/grafico/convergencia')
+def grafico_convergencia():
+    geracoes = json.load(open('temp/geracoes.json'))
+    evolucao = [g[-1] for g in geracoes]
+    graficos.grafico_convergencia(evolucao)
+    return ''
+            
+
+if __name__ == '__main__':
     app.run()
